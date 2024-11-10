6b3748e57f86c95e4e519236e1bf021e23428047
Mohamed Amin <Amin@Mohameds-MacBook-Pro.local>
2024-11-10 18:27:11 +0100

    Make repo pip installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "random",
+        "unittest"
+    ],
+    entry_points={
+        "console_scripts": [
+            "math_quiz = math_quiz.math_quiz:main"
+        ]
+    },
+    description="A simple math quiz game.",
+    author="Mohamed Amin"
+)
+
